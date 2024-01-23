d7fdc974a4dd17bc933c7e43eb2633c82087fdf1
sailakshmy <sailakshmy94@gmail.com>
2024-01-23 23:27:02 +0400

    Commit-6:- Error boundaries and error handling

--- nexttutorial/app/about/page.tsx
+++ nexttutorial/app/about/page.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Link from 'next/link';
 
 const About = () => {
+    //Throwing an error on purpose so that the error boundary can be triggered
+    // throw new Error('Not today!');
   return (
    <>
     <h1>
